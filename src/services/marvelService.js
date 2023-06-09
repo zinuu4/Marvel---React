fb403d84f040d665a903e0306792c910b357cd94
zinuu4 <unvm7721@gmail.com>
2023-06-09 20:10:27 +0300

    The layout has been implemented. The code for fetching and processing data from the server via an API and rendering a random character on the page from this data has been implemented.

---
+++ src/services/marvelService.js
@@ -0,0 +1,36 @@
+class marvelService {
+  _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+  _apiKey = 'apikey=18e46518a017d239d36c7fd777ef758b';
+
+  getResource = async (url) => {
+    let res = await fetch(url);
+
+    if(!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  }
+
+  getAllCharacters = async () => {
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    return res.data.results.map(this._transformCharacter);
+  }
+
+  getCharacter = async (id) => {
+    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    return this._transformCharacter(res.data.results[0]);
+  }
+
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description ? char.description.slice(0, 150) + '...' : char.description = 'No description for this character yet',
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url
+    }
+  }
+}
+
+export default marvelService;
